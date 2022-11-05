f7e7491c7923bc0406098bf01125ea027aa9e856
PythonXHunter <110535689+PythonXHunter@users.noreply.github.com>
2022-11-05 13:48:14 +0530

    Add files via upload

--- seta/seta2.php
+++ seta/seta2.php
@@ -1,10 +1,84 @@
 <?php
 
-include "seta2.inc";
-$txt1 = $_GET['txt1'];
 $op = $_GET['op'];
+$arr = array("Aryan","Aditya","Aman","Aamir");
+switch($op)
+{
+case "a":
+echo "Stack<br>";
+echo "Elements before push<br>";
+foreach($arr as $a)
+{
+echo "$a <br>";
+}
+array_push($arr,"adi");
+echo "Elements After push<br>";
+foreach($arr as $a)
+{
+echo "$a <br>";
+}
+break;
+case "b":
+echo "Stack<br>";
+echo "Elements before pop<br>";
+foreach($arr as $a)
+{
+echo "$a <br>";
+}
+array_pop($arr);
+echo "Elements After pop<br>";
+foreach($arr as $a)
+{
+echo "$a <br>";
+}
+break;
+case "c":
+echo "Stack<br>";
+echo "Display<br>";
+foreach($arr as $a)
+{
+echo "$a <br>";
+}
 
-if($op == 'callength')
-	echo "Length of Given String is".$op($txt1);
+break;
+case "d":
+echo "Stack<br>";
+echo "Elements before shift<br>";
+foreach($arr as $a)
+{
+echo "$a <br>";
+}
+array_shift($arr);
+echo "Elements After shift<br>";
+foreach($arr as $a)
+{
+echo "$a <br>";
+}
+break;
+case "e":
+echo "Stack<br>";
+echo "Elements before unshift<br>";
+foreach($arr as $a)
+{
+echo "$a <br>";
+}
+array_unshift($arr,"adi");
+echo "Elements After unshift<br>";
+foreach($arr as $a)
+{
+echo "$a <br>";
+}
+break;
+case "f":
+echo "Stack<br>";
+echo "Display<br>";
+foreach($arr as $a)
+{
+echo "$a <br>";
+}
+
+default:
+echo "Please choose the correct Option";
+}
 
 ?>
